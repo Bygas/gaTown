af70f289078fd307798c77fe602a6d173e52ccb5
Bygas <93877251+Bygas@users.noreply.github.com>
2026-03-16 21:21:17 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'com.games.wenzi.taoyuan',
-  appName: '桃源乡',
+  appId: 'com.gakoy.game',
+  appName: 'ga Köy',
   webDir: 'docs'
 }
 
